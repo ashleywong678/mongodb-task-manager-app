b403c33085470119754d5b6a9ff0501b29515162
Ashley <ashleywong678@gmail.com>
2019-11-29 22:37:42 -0500

    challenge to query data from mongodb using find and findOne

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,10 @@
 //CRUD - Create Read Update Delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -12,46 +12,35 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     }
 
     const db = client.db(databaseName)
-    // db.collection('users').insertOne({
-    //     name: 'Chris',
-    //     age: 26
-    // }, (error, result) => {
+
+    // db.collection('users').findOne({ _id: 'Jen' }, (error, user) => {
     //     if (error){
-    //         return console.log('Unable to insert user')
+    //         return console.log('Unable to fetch')
     //     }
-    //     console.log(result.ops)
+    //     console.log(user)
     // })
 
-//     db.collection('users').insertMany([
-//         {
-//             name: 'Jen',
-//             age: 31
-//         }, {
-//             name: 'Gunther',
-//             age: 27
-//         }
-//     ], (error, result) => {
-//         if(error){
-//             return console.log('Unable to insert docs')
-//         }
-//         console.log(result.ops)
-//     })
-
-// Challenge: Insert 3 tasks into a new tasks collection
-// 1. use insertMany to insert 3 docs
-//     - description(string), completed(boolean)
-// 2. setup the callback to handle error or print ops
-// 3. run the script
-// 4. refresh the database in Robo 3t and view data in tasks collection
-
-    db.collection('tasks').insertMany([
-        { description: 'wash dishes', completed: false},
-        { description: 'take out garbage', completed: true},
-        { description: 'feed cat', completed: true }
-    ], (error, result) => {
-        if(error){
-            return console.log('Unable to insert docs')
+    // db.collection('users').find({ age: 27 }).toArray((error, users) =>{
+    //     console.log(users)
+    // })
+    
+    // Challenge: Use find and findOne with tasks
+    // 1. use findOne to fetch the last task by its id (print doc to console)
+    // 2. use find to fetch all tasks that are not completed (print docs to console)
+    // 3. test  your work
+    
+    db.collection('tasks').findOne({ _id: new ObjectID('5de16011b55e4944a2cc3c97') }, (error, user) => {
+        if (error){
+            return console.log('Unable to fetch')
         }
-        console.log(result.ops)
+        console.log(user)
     })
+    
+    db.collection('tasks').find({ completed: false }).toArray((error, user) => {
+        if (error){
+            return console.log('Unable to fetch')
+        }
+        console.log(user)
+    })
+    
 })
