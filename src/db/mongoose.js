186efcb7ee9cb46b30b6c5efac3c7430c613f12c
Ashley <ashleywong678@gmail.com>
2019-12-01 20:19:08 -0500

    challenge to add validations to task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,13 +7,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true
 })
 
-// Challenge: Add a password field to User
-// 1. Setup the field as a required string
-// 2. Ensure the length is greater then 6
-// 3. trim password 
-// 4. ensure the password doesnt contain password
-// 5. test your work
-
 const User = mongoose.model('User', {
     name: {
         type: String,
@@ -52,29 +45,37 @@ const User = mongoose.model('User', {
     }
 })
 
-const me = new User({
-    name: '    Andrew     ',
-    email: 'and@gmail.com   ',
-    password: ' newcat12   '
+// const me = new User({
+//     name: '    Andrew     ',
+//     email: 'and@gmail.com   ',
+//     password: ' newcat12   '
+// })
+
+// me.save().then((response) => console.log(response)).catch((error) => console.log('Error: ', error))
+
+
+// Challenge: Add validation and sanitation to task
+// 1. trim the description and make it required
+// 2. make completed optional and default it to false
+// 3. test work
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true        
+    }, 
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
-me.save().then((response) => console.log(response)).catch((error) => console.log('Error: ', error))
+const task = new Task({
+    description: 'finish homework'
+    // completed: true
+})
 
-
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String
-//     }, 
-//     completed: {
-//         type: Boolean
-//     }
-// })
-
-// const task = new Task({
-//     description: 'feed cat',
-//     completed: true
-// })
-
-// task.save()
-//     .then((response) => console.log(response))
-//     .catch((error) => console.log(error))
+task.save()
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error))
