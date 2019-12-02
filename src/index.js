3c96ccd23cb40f95f3ce1407647962cb7e333248
Ashley <ashleywong678@gmail.com>
2019-12-02 07:18:50 -0500

    challenge to setup task post enpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 
 const app = express()
@@ -12,7 +13,23 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
     
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
+    }).catch((error) => {
+        res.status(400)
+        res.send(error)
+    })
+})
+
+// Challenge: setup the task creation endpoint
+// 1. create a seperate file for the task model (load it into index.js)
+// 2. create the task creation endpoint (handle success and error)
+// 3. test endpoints
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    
+    task.save().then(() => {
+        res.status(201).send(task)
     }).catch((error) => {
         res.status(400)
         res.send(error)
