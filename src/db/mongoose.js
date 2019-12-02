ee63be4812ebe618533818558a36e894cfd7ec3e
Ashley <ashleywong678@gmail.com>
2019-12-01 21:17:16 -0500

    rmved logic for user model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,58 +1,10 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Email is invalid")
-            }        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '    Andrew     ',
-//     email: 'and@gmail.com   ',
-//     password: ' newcat12   '
-// })
-
-// me.save().then((response) => console.log(response)).catch((error) => console.log('Error: ', error))
-
 
 const Task = mongoose.model('Task', {
     description: {
@@ -65,12 +17,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
-
-const task = new Task({
-    description: 'finish homework'
-    // completed: true
-})
-
-task.save()
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error))
