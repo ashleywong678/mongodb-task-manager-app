da6514df98fbc524e81fdd41629255e09f17f727
Ashley <ashleywong678@gmail.com>
2019-12-02 19:19:56 -0500

    imported task router and set app to use router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
-const userRouter = require('./routers/users')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 
 const app = express()
@@ -10,13 +9,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
+app.use(taskRouter)
 
-
-// Challenge: Create a task router
-// 1. create new file the creates/exports new router
-// 2. move all the task routes over
-// 3. load in and use that router with the express app
-// 4. test your work
 
 
 app.listen(port, () => {
