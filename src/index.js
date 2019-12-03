1a6b5e45209dd01df3a866454a76883163125ac1
Ashley <ashleywong678@gmail.com>
2019-12-03 12:45:06 -0500

    seperated middleware logic from index.js

--- src/index.js
+++ src/index.js
@@ -15,11 +15,6 @@ const port = process.env.PORT || 3000
 //     }
 // })
 
-// challenge: setup middleware for maintenance mode
-// 1. register a new middleware function
-// 2. send back a maintenance message with a 503 status code
-// 3. try your requests from the server and confirm status/message shows
-
 app.use((req, res, next) => {
     res.status(503).send('Under maintenance')
 })
