fb38b58588126f6d28c6e9ac8ef215fa1123f76d
Ashley <ashleywong678@gmail.com>
2019-12-03 13:17:09 -0500

    rmved logic for middleware

--- src/index.js
+++ src/index.js
@@ -7,19 +7,6 @@ const taskRouter = require('./routes/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if(req.method === 'GET'){
-//         res.send('GET reqests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-app.use((req, res, next) => {
-    res.status(503).send('Under maintenance')
-})
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
