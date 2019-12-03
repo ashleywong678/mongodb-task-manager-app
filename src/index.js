a22cfe6dc898a977d6f7483aea792a431d494d3d
Ashley <ashleywong678@gmail.com>
2019-12-03 12:24:16 -0500

    comopleted challenge to add middleware and customize response

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ const taskRouter = require('./routes/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET'){
+//         res.send('GET reqests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// challenge: setup middleware for maintenance mode
+// 1. register a new middleware function
+// 2. send back a maintenance message with a 503 status code
+// 3. try your requests from the server and confirm status/message shows
+
+app.use((req, res, next) => {
+    res.status(503).send('Under maintenance')
+})
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
