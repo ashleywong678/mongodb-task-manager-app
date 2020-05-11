24bb1a62087ef6fc9ca0b07b56c75752a61f48e5
Ashley <ashleywong678@gmail.com>
2020-05-10 20:46:25 -0400

    redoing lessons from node course

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,45 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+// const me = new User({ name: "Ashley", age: 27 });
+// me.save()
+//   .then(() => {
+//     console.log("me", me);
+//   })
+//   .catch((error) => {
+//     console.log("Error", error);
+//   });
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+});
+
+const task = new Task({ description: "new task", completed: false });
+task
+  .save()
+  .then(() => {
+    console.log(task);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
